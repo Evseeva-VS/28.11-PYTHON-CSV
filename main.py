95036798e68d017e7b2eeab62b74b0d33dd9cd47
210User <210User@pgk63.ru>
2022-11-29 09:23:40 +0400

    Добавлен обработчик команд пользователя

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ MENU = {
     '7': 'Вывести сводку по количеству студентов',
     '8': 'Вывести записи',
     '9': 'Сохранить в файл',
-    '0': '<-Меню',
+    '0': '<- Меню',
     'exit': 'Выход'
 }
 
@@ -17,3 +17,30 @@ def showMenu():
     print('\n'.join([f'{key}: {value}' for key, value in MENU.items()]))
 
 showMenu()
+
+while True:
+    command = input()
+    if command == '1':
+        print(1)
+    elif command == '2':
+        print(2)
+    elif command == '3':
+        print(3)
+    elif command == '4':
+        print(4)
+    elif command == '5':
+        print(5)
+    elif command == '6':
+        print(6)
+    elif command == '7':
+        print(7)
+    elif command == '8':
+        print(8)
+    elif command == '9':
+        print(9)
+    elif command == '0':
+        showMenu()
+    elif command == 'exit':
+        break
+    else:
+        print('Неверная команда, попробуйте ещё раз', '0: <- Меню', sep='\n')
